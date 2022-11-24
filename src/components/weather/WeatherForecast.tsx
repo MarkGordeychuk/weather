da42f7b100e0bf81a286b0ed7b6b1cbacccc9f6a
MarkGordeychuk <markgordeychuk@gmail.com>
2022-11-24 22:59:39 +0500

    Update WeatherForecast.tsx

--- src/components/weather/WeatherForecast.tsx
+++ src/components/weather/WeatherForecast.tsx
@@ -4,30 +4,7 @@ import WeatherForecastChart from "./WeatherForecastChart";
 import WeatherForecastList from "./WeatherForecastList";
 
 
-
-type CityObj = {
-    id: number,
-    name: string,
-    coord: {
-        lat: number,
-        lon: number
-    },
-    country: string,
-    population: number,
-    timezone: number,
-    sunrise: number,
-    sunset: number
-};
-
-type WeatherResponse = {
-    cod: number,
-    list: Array<any>,
-    city: object
-};
-
 function WeatherForecast({ data }) {
-    // let [temperature, setTemperature] = React.useState(data.main.temp);
-
 
     return (
         <>
