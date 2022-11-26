189bf98f1e87317adbad4fbe104825127cc64c1a
MarkGordeychuk <markgordeychuk@gmail.com>
2022-11-26 23:25:02 +0500

    Update Weather.tsx

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -8,40 +8,71 @@ import WeatherForecast from "./WeatherForecast";
 // import forecast from "../../examples/forecast.json";
 
 
-export default function Weather(props) {
-    let [currentWeather, setCurrentWeather] = React.useState(null);
-    let [weatherForecast, setWeatherForecast] = React.useState(null);
+type WeatherProps = {
+    cityID: number
+};
 
-    if (!currentWeather) axios.get(
-        "https://api.openweathermap.org/data/2.5/weather",
-        {
-            params: {
-                "id": props.cityID,
-                "appid": "4c87a0ac930069319c518bad48cef361"
+type WeatherState = {
+    currentWeather: any,
+    weatherForecast: any
+};
+
+
+class Weather extends React.Component<WeatherProps, WeatherState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentWeather: null,
+            weatherForecast: null
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.currentWeather ? <CurrentWeather data={this.state.currentWeather}/> : null}
+                {this.state.weatherForecast ? <WeatherForecast data={this.state.weatherForecast}/> : null}
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        this.updateWeatherInfo();
+    }
+
+    updateWeatherInfo() {
+        // Test
+        // this.setState({
+        //     currentWeather: weather,
+        //     weatherForecast: forecast
+        // });
+
+        axios.get(
+            "https://api.openweathermap.org/data/2.5/weather",
+            {
+                params: {
+                    "id": this.props.cityID,
+                    "appid": "4c87a0ac930069319c518bad48cef361"
+                }
             }
-        }
-    ).then(response => {
-        console.log(response.data);
-        setCurrentWeather(response.data);
-    });
+        ).then(response => {
+            console.log(response.data);
+            this.setState({currentWeather: response.data});
+        });
 
-    if (!weatherForecast) axios.get(
-        "https://api.openweathermap.org/data/2.5/forecast",
-        {
-            params: {
-                "id": props.cityID,
-                "appid": "4c87a0ac930069319c518bad48cef361"
+        axios.get(
+            "https://api.openweathermap.org/data/2.5/forecast",
+            {
+                params: {
+                    "id": this.props.cityID,
+                    "appid": "4c87a0ac930069319c518bad48cef361"
+                }
             }
-        }
-    ).then(response => {
-        console.log(response.data);
-        setWeatherForecast(response.data);
-    });
-
-    return (
-        <div>
-            { currentWeather ? <CurrentWeather data={currentWeather} /> : null }
-            { weatherForecast ? <WeatherForecast data={weatherForecast} /> : null }
-        </div>
-    );
+        ).then(response => {
+            console.log(response.data);
+            this.setState({weatherForecast: response.data});
+        });
+    }
 }
+
+export default Weather;
